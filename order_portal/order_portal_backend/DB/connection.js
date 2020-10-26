5a8ed8360a3346d1420a6d664373787481a9f096
mittal4all <ankitmittal4all@gmail.com>
2020-10-26 11:51:11 +0530

    database url change

--- order_portal/order_portal_backend/DB/connection.js
+++ order_portal/order_portal_backend/DB/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-const URI = "mongodb+srv://dbUser:dbUser@cluster0.0su2c.mongodb.net/test?retryWrites=true&w=majority";
+const URI = "Your Database Url";
 const connectDB = async () => {
     await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('database connected');
 }
 
 module.exports = connectDB;
+
